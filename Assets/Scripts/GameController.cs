fb844d7880a829079a7c2decf4608c3cc39a7c12
Nguyễn Đức Học <hoc982002@gmail.com>
2024-01-16 13:43:24 +0700

    abc

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject obstacle;
+    public float  spawnTime;
+    float m_spawnTime;
+    int m_score;
+    bool  m_isGameover;
+    UIManager m_ui;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_spawnTime = 0;
+        m_ui = FindObjectOfType<UIManager>();
+        m_ui.SetScoreText("Score : "+m_score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_isGameover)
+        {
+            m_spawnTime = 0;
+            m_ui.ShowGameoverPanel(true);
+            return;
+        }
+        m_spawnTime -= Time.deltaTime;
+        if(m_spawnTime <=0)
+        {
+            SpawnObstacle();
+            m_spawnTime = spawnTime;
+        }
+    }
+
+    public void SpawnObstacle()
+    {
+        float ranYpos = Random.Range(-2.75f,-1f);
+        Vector2 spawnPos = new Vector2(11,ranYpos);
+        if(obstacle)
+        {
+            Instantiate(obstacle,spawnPos,Quaternion.identity);
+        } 
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void SetScore(int value)
+    {
+        m_score = value;
+    }
+    public int GetScore()
+    {
+        return m_score;
+    }
+    public void ScoreIncrement()
+    {
+        if(m_isGameover)
+            return;
+        m_score++;
+        m_ui.SetScoreText("Score : "+m_score);
+    }
+    public  bool IsGameover()
+    {
+        return m_isGameover;
+    }
+    public void SetGameoverState(bool state)
+    {
+        m_isGameover = state;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce;
+    Rigidbody2D m_rb;
+    bool m_isGround;
+    GameController m_gc;
+
+    public AudioSource aus;
+    public AudioClip jumpSound;
+    public AudioClip loseSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_gc = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isJumpKeyPressed = Input.GetKeyDown(KeyCode.Space);
+        if (isJumpKeyPressed && m_isGround && !m_gc.IsGameover()) 
+        {
+            m_rb.AddForce(Vector2.up * jumpForce );
+            m_isGround = false;
+
+            if(aus && jumpSound)
+            {
+                aus.PlayOneShot(jumpSound);
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground"))    
+        {
+            m_isGround=true;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Obstacle"))
+        {
+            m_gc.SetGameoverState(true);
+            Debug.Log("...");
+
+            if(aus && loseSound)
+            {
+                aus.PlayOneShot(loseSound);
+            }
+        }
+    }
+}
